9533a9aabae3057eea0627481d1aceed4ae3ab1f
u16ptr <u16ptr@proton.me>
2024-08-29 12:12:23 +0200

    Adding and setting up Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
   {
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
